07474ba9c3cca42e308dbb2a3379769542b7f6b3
koncc1 <volk81498@gmail.com>
2026-07-13 19:40:51 +0200

    TASK "16. Create User DAO"

--- robota/app/src/main/java/com/example/it_robota/database/UserDao.java
+++ robota/app/src/main/java/com/example/it_robota/database/UserDao.java
@@ -9,18 +9,19 @@ import com.example.it_robota.models.User;
 @Dao
 public interface UserDao {
 
+    // Add a new user to the database
     @Insert
     void insertUser(User user);
 
-
+    // Get user by email
     @Query("SELECT * FROM users WHERE email = :email LIMIT 1")
     User getUserByEmail(String email);
 
-
+    // Get user by ID
     @Query("SELECT * FROM users WHERE id = :id LIMIT 1")
     User getUserById(long id);
 
-
+    // Check if a user with this email already exists
     @Query("SELECT EXISTS(SELECT 1 FROM users WHERE email = :email)")
     boolean checkUserExists(String email);
 }
