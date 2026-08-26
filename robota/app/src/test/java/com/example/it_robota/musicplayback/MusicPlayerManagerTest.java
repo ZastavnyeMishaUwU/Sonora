178725543a01061efe8c1870e6c7a8ac97b215df
mentos <olgrv36@gmail.com>
2026-08-26 20:08:10 +0300

    session manager unit tests done

--- robota/app/src/test/java/com/example/it_robota/musicplayback/MusicPlayerManagerTest.java
+++ robota/app/src/test/java/com/example/it_robota/musicplayback/MusicPlayerManagerTest.java
@@ -7,10 +7,7 @@ import org.mockito.junit.MockitoJUnitRunner;
 
 import java.io.IOException;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
